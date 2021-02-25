a43d3008b67e04f25eeeaa0f9b43bb5459c0ef93
Erik Moratori <emoratori@yahoo.com.br>
2021-02-25 09:14:58 -0300

    Criando lógica

--- js/main.js
+++ js/main.js
@@ -1,15 +1,54 @@
 var display = 0;
 var pilha = 0;
+var produto = 0;
 var sinal = "";
+var flagOp = true;
 
 function numero(parametro){
     display = display * 10 + parseInt(parametro.id, 10);
     document.getElementById("display").value = display;
+    flagIgual=false;
+    llog("numero");
 }
 
 function operacao(parametro){
     pilha = display;
-    display = 0;
+    if (display != null){
+        igual("operacao");
+    }
+    display = null;
     sinal = parametro.id;
-    console.log(display);
+    llog("operacao");
 }
+
+function igual(){
+    if (sinal == "+"){
+        produto = pilha + display}
+    else if (sinal == "-"){
+        produto = pilha - display}
+    else if (sinal == "x"){
+        produto = pilha * display}
+    else if (sinal == "/"){
+        produto = pilha / display;
+    }
+    document.getElementById("display").value = produto;
+    llog("=");
+}
+
+function c(){
+    display = 0;
+    pilha = 0;
+    produto = 0;
+    sinal="";
+    flagIgual = true;
+    document.getElementById("display").value = produto;
+    llog("c");
+}
+
+function llog(par){
+    console.log(par + " _____");
+    console.log("display: " + display)
+    console.log("pilha:   " + pilha);
+    console.log("produto: " + produto);
+    console.log('sinal: " ' + sinal +'"');
+}   console.log("flagOp:  " + flagOp);
